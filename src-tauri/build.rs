7ab449d98f167b33533a98b48518d0ffcbbd762b
jcdurante3-crypto <jc.durante3@gmail.com>
2026-08-09 23:11:38 +0800

    Okeeeee

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,3 @@
 fn main() {
-    // Skip tauri-build on Windows to avoid icon.ico format issues
-    #[cfg(not(target_os = "windows"))]
-    tauri_build::build();
-    
-    #[cfg(target_os = "windows")]
-    {
-        // For Windows, we'll handle icon generation differently
-        // This prevents RC.EXE errors with malformed ICO files
-        println!("cargo:warning=Icon will be embedded via alternative method on Windows");
-    }
+    tauri_build::build()
 }
